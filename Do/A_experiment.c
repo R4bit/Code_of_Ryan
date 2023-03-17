12336b7e98ea40e8e8e69d66aa7fd73d7b4694d5
Katanaexclusived <renyvannnn@gmail.com>
2023-03-17 18:49:38 +0800

    11

---
+++ Do/A_experiment.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int b ;
+    double a , sum ;
+    char c ;
+    
+    scanf("%lf , %d , %c" , &a , &b , &c );
+    printf("sun is %lf \n" , a + b + c );
+    printf(" \n") ;
+ 
+}
