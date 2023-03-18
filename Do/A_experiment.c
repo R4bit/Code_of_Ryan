51da7ac342d903a4f432bd8c28762af1ea1153cd
Katanaexclusived <renyvannnn@gmail.com>
2023-03-18 16:33:45 +0800

    new

--- Do/A_experiment.c
+++ Do/A_experiment.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
-void main()
-{
-    int b ;
-    double a , sum ;
-    char c ;
-    
-    scanf("%lf , %d , %c" , &a , &b , &c );
-    printf("sun is %lf \n" , a + b + c );
-    printf(" \n") ;
-    printf(" \n") ;
-     
+
+int main() {
+    int arr[2][3] = { {1, 2, 3}, {4, 5, 6} };
+    int **p;   // 定义一个指向指针的指针变量
+    int i, j;
+
+    // 将二级指针指向二维数组的第一个元素的地址
+    p = (int **)arr;
+
+    // 使用二级指针来遍历二维数组
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", *((int *)p + i * 3 + j));
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
