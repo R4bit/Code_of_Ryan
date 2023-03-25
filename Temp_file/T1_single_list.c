6394fbe82e9023fed9d6f8070cb60579f1abbef0
Katanaexclusived <renyvannnn@gmail.com>
2023-03-25 13:39:14 +0800

    new

--- Temp_file/T1_single_list.c
+++ Temp_file/T1_single_list.c
@@ -19,6 +19,49 @@ struct node
 struct node *head = NULL;
 struct node *current = NULL;
 
+
+//delete a link with given key
+struct node* delete(int key) 
+{
+
+   //start from the first link
+   struct node* current = head;
+   struct node* previous = NULL;
+	
+   //if list is empty
+   if(head == NULL) 
+   {
+      return NULL;
+   }
+
+   // traverse list :
+   while(current->key != key) 
+   {
+      //if it is last node
+      if(current->next == NULL) 
+      {
+         return NULL;
+      } 
+      else 
+      {         // go on :
+         previous = current;
+         current = current->next;
+      }
+   }
+
+   //found a match, update the link
+   if(current == head)  //case that need to delete the first node : just bypass the "head".
+   {
+      head = head->next;
+   }
+   else  // bypass the current link.
+   {  
+      previous->next = current->next;
+   }    
+	
+   return current;
+}
+
 //display the list.
 void printList() 
 {
@@ -56,37 +99,10 @@ void insertFirst(int key, int data)
    //printf("current head address: \n%p\n", head);
 }
 
-//delete first item
-struct node* deleteFirst() 
-{
-
-   //save reference to first link
-   struct node *tempLink = head;
-	
-   //mark next to first link as first 
-   head = head->next;
-	
-   //return the deleted link
-   return tempLink;
-}
-
 //is list empty
 bool isEmpty() 
 {
    return head == NULL;
-}
-
-int length() 
-{
-   int length = 0;
-   struct node *current;
-	
-   for(current = head; current != NULL; current = current->next) 
-   {
-      length++;
-   }
-	
-   return length;
 }
 
 //find a link with given key
@@ -119,103 +135,6 @@ struct node* find(int key)
    return current;
 }
 
-//delete a link with given key
-struct node* delete(int key) 
-{
-
-   //start from the first link
-   struct node* current = head;
-   struct node* previous = NULL;
-	
-   //if list is empty
-   if(head == NULL) 
-   {
-      return NULL;
-   }
-
-   //navigate through list
-   while(current->key != key) 
-   {
-
-      //if it is last node
-      if(current->next == NULL) {
-         return NULL;
-      } else {
-         //store reference to current link
-         previous = current;
-         //move to next link
-         current = current->next;
-      }
-   }
-
-   //found a match, update the link
-   if(current == head) 
-   {
-      //change first to point to next link
-      head = head->next;
-   } else {
-      //bypass the current link
-      previous->next = current->next;
-   }    
-	
-   return current;
-}
-
-void sort() 
-{
-
-   int i, j, k, tempKey, tempData;
-   struct node *current;
-   struct node *next;
-	
-   int size = length();
-   k = size ;
-	
-   for ( i = 0 ; i < size - 1 ; i++, k-- ) 
-   {
-      current = head;
-      next = head->next;
-		
-      for ( j = 1 ; j < k ; j++ ) 
-      {   
-
-         if ( current->data > next->data ) 
-         {
-            // switch data
-            tempData = current->data;
-            current->data = next->data;
-            next->data = tempData;
-
-            // switch key
-            tempKey = current->key;
-            current->key = next->key;
-            next->key = tempKey;
-         }
-			
-         current = current->next;
-         next = next->next;
-      }
-   }   
-}
-
-void reverse(struct node** head_ref) 
-{
-   struct node* prev   = NULL;
-   struct node* current = *head_ref;
-   struct node* next;
-	
-   while (current != NULL) 
-   {
-      // switch address
-      next  = current->next;
-      current->next = prev;   
-      prev = current;
-      current = next;
-   }
-	
-   *head_ref = prev;
-}
-
 void main() 
 {
    // insert node from the beginning
@@ -225,68 +144,28 @@ void main()
    insertFirst(4,40);
    insertFirst(5,50);
    insertFirst(6,60); 
-
-   printf("Original List: "); 
-	
-   //print list
-   printList();
-
-   while(!isEmpty()) {            
-      struct node *temp = deleteFirst();
-      printf("\nDeleted value:");
-      printf("(%d,%d) ",temp->key,temp->data);
-   }  
-	
-   printf("\nList after deleting all items: ");
-   printList();
-
-   // insert node from the beginning
    insertFirst(7,70);
    insertFirst(8,80);
    insertFirst(9,90);
    insertFirst(10,100);
-   insertFirst(11,110);
-   insertFirst(12,120);
    
-   printf("\nRestored List: ");
+   printf("Original List: "); 
    printList();
    printf("\n");  
+   printf("\n");  
 
-   struct node *foundLink = find(10);
-	
-   if(foundLink != NULL) {
-      printf("Element found: ");
-      printf("(%d,%d) ",foundLink->key,foundLink->data);
-      printf("\n");  
-   } else {
-      printf("Element not found.");
-   }
-
-   delete(10);
+   delete(6);
    printf("List after deleting an item: ");
    printList();
    printf("\n");
-   foundLink = find(4);
-	
-   if(foundLink != NULL) {
-      printf("Element found: ");
-      printf("(%d,%d) ",foundLink->key,foundLink->data);
-      printf("\n");
-   } else {
-      printf("Element not found.");
-   }
-	
    printf("\n");
-   sort();
-	
-   printf("List after sorting the data: ");
-   printList();
-	
-   reverse(&head);
-   printf("\nList after reversing the data: ");
-   printList();
 
-   printf("\nSingle linked list programmed......");
+   printf("List after adding an item : ");
+   printList();
+   printf("\n");
+   printf("\n");
+
+   printf("deleting and adding operation successfully achieved !!! \n");
 
 }
 
